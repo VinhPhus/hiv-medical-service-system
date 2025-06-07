198821f6f290a7765df73ed0c2596c9091f4ce81
Huỳnh Nhất Huy <nhathuyhuynh240@gmail.com>
2025-06-07 15:37:35 +0700

    Update TestType.java

--- backend/src/main/java/com/hivcare/enums/TestType.java
+++ backend/src/main/java/com/hivcare/enums/TestType.java
@@ -1,5 +1,14 @@
 package main.java.com.hivcare.enums;
 
 public enum TestType {
-
+    CD4_COUNT,
+    VIRAL_LOAD,
+    HEMOGLOBIN,
+    CREATININE,
+    ALT,
+    AST,
+    CHOLESTEROL,
+    GLUCOSE,
+    HIV_ANTIBODY,
+    HIV_RNA
 }
