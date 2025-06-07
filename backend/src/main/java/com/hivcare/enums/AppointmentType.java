5374c68a6ae073e15773e4a7694b0a979a7d6e14
Huỳnh Nhất Huy <nhathuyhuynh240@gmail.com>
2025-06-07 15:35:58 +0700

    Update AppointmentType.java

--- backend/src/main/java/com/hivcare/enums/AppointmentType.java
+++ backend/src/main/java/com/hivcare/enums/AppointmentType.java
@@ -1,5 +1,13 @@
 package main.java.com.hivcare.enums;
 
 public enum AppointmentType {
+    CONSULTATION,
+    FOLLOW_UP,
+    TEST_RESULTS,
+    COUNSELING,
+    EMERGENCY,
+    MEDICATION_REVIEW,
+    ANONYMOUS_CONSULTATION
+    
 
 }
