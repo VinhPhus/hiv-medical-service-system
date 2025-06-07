42ee3d6cf832198ff77844af861d68cc8c8b873d
Huỳnh Nhất Huy <nhathuyhuynh240@gmail.com>
2025-06-07 15:35:22 +0700

    Update AppointmentStatus.java
    update

--- backend/src/main/java/com/hivcare/enums/AppointmentStatus.java
+++ backend/src/main/java/com/hivcare/enums/AppointmentStatus.java
@@ -1,5 +1,12 @@
 package main.java.com.hivcare.enums;
 
 public enum AppointmentStatus {
+    SCHEDULED,
+    CONFIRMED,
+    IN_PROGRESS,
+    COMPLETED,
+    CANCELLED,
+    NO_SHOW,
+    RESCHEDULED
 
 }
