68fe45cb057b4f2f11194e76f68148aecb7aca22
NhatDuy <duylnn1177@ut.edu.vn>
2025-07-07 23:52:11 +0700

    Update ResourceNotFoundException.java

--- backend/HIV_test/backend/src/main/java/com/hivcare/exception/ResourceNotFoundException.java
+++ backend/HIV_test/backend/src/main/java/com/hivcare/exception/ResourceNotFoundException.java
@@ -2,7 +2,9 @@ package com.hivcare.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
     
-  
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
     
     public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
         super(String.format("Không tìm thấy %s với %s: '%s'", resourceName, fieldName, fieldValue));
