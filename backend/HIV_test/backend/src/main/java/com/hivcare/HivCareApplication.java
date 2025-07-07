3bb64faa0aefa59ca4501106b2adae737096eafd
NhatDuy <duylnn1177@ut.edu.vn>
2025-07-07 23:54:40 +0700

    Update HivCareApplication.java

--- backend/HIV_test/backend/src/main/java/com/hivcare/HivCareApplication.java
+++ backend/HIV_test/backend/src/main/java/com/hivcare/HivCareApplication.java
@@ -5,12 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
-@EnableAsync
-@EnableScheduling
-public class HivCareApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(HivCareApplication.class, args);
+
     }
     
 }
