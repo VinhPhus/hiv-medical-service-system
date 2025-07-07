7635e7c74c1227127150935f581e3f3a7c6829e8
NhatDuy <duylnn1177@ut.edu.vn>
2025-07-07 23:52:36 +0700

    Update UnauthorizedException.java

--- backend/HIV_test/backend/src/main/java/com/hivcare/exception/UnauthorizedException.java
+++ backend/HIV_test/backend/src/main/java/com/hivcare/exception/UnauthorizedException.java
@@ -1,5 +1,9 @@
 package com.hivcare.exception;
 
 public class UnauthorizedException extends RuntimeException {
-    
+     public UnauthorizedException(String message) {
+        super(message);
+    }
+}
+
    
