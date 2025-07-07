3cc61ee690ae9776da0e2e3c9fc70154359c96b7
NhatDuy <duylnn1177@ut.edu.vn>
2025-07-07 23:50:38 +0700

    Update BadRequestException.java

--- backend/HIV_test/backend/src/main/java/com/hivcare/exception/BadRequestException.java
+++ backend/HIV_test/backend/src/main/java/com/hivcare/exception/BadRequestException.java
@@ -1,1 +1,9 @@
+package com.hivcare.exception;
+
+public class BadRequestException extends RuntimeException {
+    
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
 
