b9d9455b177dcd12861461c34c03c717d5786345
NhatDuy <duylnn1177@ut.edu.vn>
2025-07-08 00:02:21 +0700

    Update ArvProtocolRepository.java

--- backend/HIV_test/backend/src/main/java/com/hivcare/repository/ArvProtocolRepository.java
+++ backend/HIV_test/backend/src/main/java/com/hivcare/repository/ArvProtocolRepository.java
@@ -1,6 +1,12 @@
 package com.hivcare.repository;
 
-
+import com.hivcare.entity.ArvProtocol;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
